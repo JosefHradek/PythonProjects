d9eebd4d5b9cb4bb39788ba8fbb78da62972172b
D32\zak <spseplzen@spseplzen.cz>
2025-11-07 12:22:59 +0100

    zprava

---
+++ seznam.py
@@ -0,0 +1,18 @@
+promenna = [1, 2, 3, 4, 5]
+
+print(promenna)
+
+p2 = [1, "abc", 5.5, True, [1,2,"a"]]
+print(p2)
+
+print(p2[4])
+print(type(p2[4][1]))
+print(type(p2))
+print(p2[4][1])
+print(p2[1:4])
+
+x = [1, 2, 8, 4, 6, 11, 7, 4]
+
+for i in range(len(x)):
+    if i % 2 == 0:
+        print(x[i])
---
+++ stringy_priklady.py
@@ -0,0 +1,42 @@
+promenna = "Ahoj Karle, jak se máš?"
+
+for i in range(len(promenna)):
+    print(promenna[i])
+
+for i in range(len(promenna)):
+    print(promenna[-i-1])
+
+print("pr3******************")
+
+
+for i in range(len(promenna)):
+    print(promenna[:i+1])
+
+print("pr4******************")
+
+for i in range(len(promenna)-1):
+    print(promenna[i:2+i])
+
+print("pr5******************")
+
+for i in range(int(len(promenna)/2)+1):
+    print(promenna[i],promenna[-i-1])
+
+print("pr6****************************")
+
+a = "5"
+print(a.zfill(5))
+
+z = 10
+while z < 5
+    print
+# promena.strip()   maže mezery
+
+
+
+
+
+
+
+
+
