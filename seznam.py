6962e28779b70794a845deaf9402ff281d2088de
D32\zak <spseplzen@spseplzen.cz>
2025-11-21 12:25:36 +0100

    zprava

--- seznam.py
+++ seznam.py
@@ -96,3 +96,68 @@ for i in range(len(pole)-1,-1,-1):
     nove_pole.append(pole[i])
 
 print(nove_pole)
+
+print("pr7")
+
+pole = [5, 2, 9, 1, 7, 3, 10, 6, 4]
+pole2 = [3, 5, 4, 7, 5, 3, 4, 5, 10]
+nove_pole = []
+
+
+for i in range(len(pole)):
+    nove_pole.append(pole[i] + pole2[i])
+
+print(nove_pole)
+
+print("pr8")
+
+pole = [5, 2, 9, 1, 7, 3, 10, 6, 4]
+pole2 = [3, 5, 4, 7, 5, 3, 4, 5, 10]
+nove_pole = []
+
+
+for i in range(len(pole)):
+    nove_pole.append(pole[i] + pole2[-i-1])
+
+print(nove_pole)
+
+print("pr9")
+
+druhy_prvek = -10000000                 #float('-inf')
+prvni_prvek = -10000000             #float('-inf')
+
+pole = [2, 2, 9, 1, 7, 3, 6, 7, 4]
+
+for i in range (len(pole)):
+    if pole[i] > prvni_prvek:
+        druhy_prvek = prvni_prvek
+        prvni_prvek = pole[i]
+    elif pole[i] > druhy_prvek:
+        druhy_prvek = pole[i]
+
+print(druhy_prvek)
+print(prvni_prvek)
+
+print("pr10")
+
+pole = [2, 2, 9, 1, 7, 3, 6, 7, 4]
+je = True
+for i in range(len(pole)):
+    if pole[i] > pole[i+1]:
+        je = False
+        break
+if je:     # =if je == True
+    print(je)
+else:           # =elif je == False
+    print(je)
+print("pr_bonus")
+
+pole = [2, 2, 4, 1, 7, 3, 6, 7, 4]
+pocet_sudych = 0
+lichy = 0
+for i in range(len(pole)):
+    if pole[i]%2 == 0:
+        pocet_sudych += 1
+    else:
+        lichy += 1
+print(pocet_sudych,",",lichy)
